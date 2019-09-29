a92bef266edcc54d551b008e89f895f1e7b1a566
Black-Horse-73 <retro_73@ukr.net>
2019-09-29 21:15:59 +0300

    1

---
+++ src/app/collection/collection.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-collection',
+  templateUrl: './collection.component.html',
+  styleUrls: ['./collection.component.css']
+})
+export class CollectionComponent {
+
+  films;
+
+  constructor() {
+
+    this.films = [
+      { poster: '../../assets/img/ivi.jpg', title: 'Игра в иммитацию', yearOfIssue: 2019 },
+      { poster: '../../assets/img/sr.jpg', title: 'Славные ребята', yearOfIssue: 1990 },
+      { poster: '../../assets/img/g.jpg', title: 'Гангстер', yearOfIssue: 2007 },
+      { poster: '../../assets/img/dkp.jpg', title: 'Далласский клуб покупателей', yearOfIssue: 2013 },
+      { poster: '../../assets/img/t.jpeg', title: 'Терминал', yearOfIssue: 2004 },
+      { poster: '../../assets/img/inp.jpg', title: 'Игра на понижение', yearOfIssue: 2015 },
+      { poster: '../../assets/img/с.jpg', title: 'Кокаин', yearOfIssue: 2001 },
+      { poster: '../../assets/img/e.jpg', title: 'Эксперимент', yearOfIssue: 2010 }
+    ]
+  }
+
+  deleteFilm(i) {
+    this.films.splice(i, 1)
+  }
+  addFilm(poster, title, yearOfIssue) {
+    this.films.push(poster.value)
+    poster.value = '';
+    this.films.push(title.value)
+    title.value = '';
+    this.films.push(yearOfIssue.value)
+    yearOfIssue.value = '';
+  }
+}
